501ea3c0cf5d7ad2a507052f8888108fd8577c25
dv-loginov <dilog@rambler.ru>
2022-10-17 12:40:17 +0300

    feat: dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const res = {};
+  for (let domain of domains) {
+    const domainParts = domain.split('.').reverse();
+    let key = '';
+    for (let elem of domainParts) {
+      key += '.' + elem;
+      res[key] = res[key] 
+                  ? res[key] + 1 
+                  : 1;
+    }
+  }
+  return res;
 }
 
 module.exports = {
