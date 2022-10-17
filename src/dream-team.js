622a153ea90618cf21b3185050dbf55726288919
dv-loginov <dilog@rambler.ru>
2022-10-17 12:54:44 +0300

    feat: add dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  let res;
+  res =  members.filter(item => typeof item == 'string');
+  // console.log(res);
+  res = res.map(item => item.trim().toUpperCase()[0]);
+  // console.log(res);
+  res = res.sort();
+  // console.log(res);
+  res = res.join('');
+  // console.log(res);
+  return res;
 }
 
 module.exports = {
